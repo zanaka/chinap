42d79b0073abe91b2196d3519f1b02f155f6c86b
zanaka <e155752@ie.u-ryukyu.ac.jp>
2018-02-15 20:13:35 +0900

    インスタンスが持つプロパティの名前を出力

--- index.php
+++ index.php
@@ -1,11 +1,18 @@
 <?php
 // Menuクラスの定義
 class Menu {
-
+    public $name;
 }
 
 // Menuクラスのインスタンスを各変数に代入
 $curry = new Menu();
 $pasta = new Menu();
+//インスタンスが持つプロパティにアクセスして名前を定義
+$curry->name = 'CURRY';
+$pasta->name = 'PASTA';
+
+echo $curry->name;
+echo '<br>';
+echo $pasta->name;
 
 ?>
