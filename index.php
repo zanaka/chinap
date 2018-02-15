942499410eab9f9c40c00e8d5efe17f99df1ae0d
zanaka <e155752@ie.u-ryukyu.ac.jp>
2018-02-15 21:20:10 +0900

    priceとimageの表示

--- index.php
+++ index.php
@@ -13,7 +13,11 @@
     <h1 class="logo">茶処真壁ちなー</h1>
     <div class="menu-items">
         <?php foreach ($menus as $menu): ?>
-            <h3><?php echo $menu->name ?></h3>
+            <div class="menu-item">
+                <img src="<?php echo $menu->image ?>">
+                <h3 class="menu-item-name"><?php echo $menu->name ?></h3>
+                <p class="price">¥<?php echo $menu->price ?></p>
+            </div>
         <?php endforeach ?>
     </div>
 </div>
