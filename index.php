fbef486df337580c74c675fd3377c4bb2eb8f08a
zanaka <e155752@ie.u-ryukyu.ac.jp>
2018-02-15 20:29:56 +0900

    コンストラクタでnameプロパティをセット

--- index.php
+++ index.php
@@ -1,22 +1,20 @@
 <?php
-// Menuクラスの定義
 class Menu {
     public $name;
+
+    public function __construct($name) {
+        $this->name = $name;
+    }
 
     public function hello() {
         echo '私は'.$this->name.'です';
     }
 }
 
-// Menuクラスのインスタンスを各変数に代入
-$curry = new Menu();
-$pasta = new Menu();
-//インスタンスが持つプロパティにアクセスして名前を定義
-$curry->name = 'CURRY';
-$pasta->name = 'PASTA';
+$curry = new Menu('CURRY');
+$pasta = new Menu('PASTA');
 
 $curry->hello();
 echo '<br>';
 $pasta->hello();
-
 ?>
