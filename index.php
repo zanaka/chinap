3f4311ae3843991ae215546209b5f395b61922df
zanaka <e155752@ie.u-ryukyu.ac.jp>
2018-02-15 20:22:59 +0900

    $thisで呼び出し先のインスタンスに置き換えるメソッドを追加

--- index.php
+++ index.php
@@ -3,8 +3,8 @@
 class Menu {
     public $name;
 
-    function hello() {
-        echo '私はMenuクラスのインスタンスです';
+    public function hello() {
+        echo '私は'.$this->name.'です';
     }
 }
 
@@ -14,10 +14,6 @@ $pasta = new Menu();
 //インスタンスが持つプロパティにアクセスして名前を定義
 $curry->name = 'CURRY';
 $pasta->name = 'PASTA';
-
-echo $curry->name;
-echo '<br>';
-echo $pasta->name;
 
 $curry->hello();
 echo '<br>';
