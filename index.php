89ec371b49ce4c5e5561ae9fa8b815ffe0512313
zanaka <e155752@ie.u-ryukyu.ac.jp>
2018-02-15 20:52:08 +0900

    ちなーのメニューを表示して，cssを当てた

--- index.php
+++ index.php
@@ -11,10 +11,29 @@ class Menu {
     }
 }
 
-$curry = new Menu('CURRY');
-$pasta = new Menu('PASTA');
-
-$curry->hello();
-echo '<br>';
-$pasta->hello();
+$soba = new Menu('そば');
+$ysoba = new Menu('やさいそば');
+$ssoba = new Menu('皿そば');
+$nsoba = new Menu('肉もやしそば');
+$menus = array ($soba,$ysoba,$ssoba,$nsoba);
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>茶処真壁ちなー</title>
+    <link rel="stylesheet" type="text/css" href="stylesheet.css">
+    <link href='https://fonts.googleapis.com/css?family=Pacifico|Lato' rel='stylesheet' type='text/css'>
+</head>
+<body>
+<div class="menu-wrapper container">
+    <h1 class="logo">茶処真壁ちなー</h1>
+    <div class="menu-items">
+        <?php foreach ($menus as $menu): ?>
+            <h3><?php echo $menu->name ?></h3>
+        <?php endforeach ?>
+    </div>
+</div>
+</body>
+</html>
