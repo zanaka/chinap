a7db81830ccbd3b08395b2147ce65d071eca172d
zanaka <e155752@ie.u-ryukyu.ac.jp>
2018-02-15 21:19:39 +0900

    priceとimageの追加

--- data.php
+++ data.php
@@ -1,10 +1,11 @@
 <?php
 require_once('menu.php');
 
-$soba = new Menu('そば');
-$ysoba = new Menu('やさいそば');
-$ssoba = new Menu('皿そば');
-$nsoba = new Menu('肉もやしそば');
+$soba = new Menu('そば',600,'https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/php/juice.png');
+$ysoba = new Menu('やさいそば',500,'https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/php/coffee.png');
+$ssoba = new Menu('皿そば',900,'https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/php/curry.png');
+$nsoba = new Menu('肉もやしそば',1200,'https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/php/pasta.png');
+
 
 $menus = array ($soba,$ysoba,$ssoba,$nsoba);
 ?>
