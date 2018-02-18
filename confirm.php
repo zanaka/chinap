1ce9c2572cbfd2d48f518a21a722c52a319c0bf4
zanaka <e155752@ie.u-ryukyu.ac.jp>
2018-02-18 23:36:51 +0900

    注文個数と金額の表示

--- confirm.php
+++ confirm.php
@@ -12,13 +12,17 @@
 <div class="order-wrapper">
     <h2>注文内容確認</h2>
     <?php foreach ($menus as $menu): ?>
-        <?php $orderCount = $_POST[$menu->getName()] ?>
+        <?php
+        $orderCount = $_POST[$menu->getName()];
+        $menu->setOrderCount($orderCount);
+        ?>
         <p class="order-amount">
             <?php echo $menu->getName() ?>
             x
             <?php echo $orderCount ?>
             個
         </p>
+        <p class="order-price"><?php echo $menu->getTotalPrice() ?>円</p>
     <?php endforeach ?>
 </div>
 </body>
