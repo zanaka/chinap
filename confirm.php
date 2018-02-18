0a438755b90181994285e9a2b1ea18b830f00e93
zanaka <e155752@ie.u-ryukyu.ac.jp>
2018-02-18 23:41:57 +0900

    注文個数に合わせた合計金額を表示されるようにした

--- confirm.php
+++ confirm.php
@@ -11,10 +11,12 @@
 <body>
 <div class="order-wrapper">
     <h2>注文内容確認</h2>
+    <?php $totalPayment = 0 ?>
     <?php foreach ($menus as $menu): ?>
         <?php
         $orderCount = $_POST[$menu->getName()];
         $menu->setOrderCount($orderCount);
+        $totalPayment += $menu->getTotalPrice();
         ?>
         <p class="order-amount">
             <?php echo $menu->getName() ?>
@@ -24,6 +26,7 @@
         </p>
         <p class="order-price"><?php echo $menu->getTotalPrice() ?>円</p>
     <?php endforeach ?>
+    <h3>合計金額: <?php echo $totalPayment ?>円</h3>
 </div>
 </body>
 </html>
